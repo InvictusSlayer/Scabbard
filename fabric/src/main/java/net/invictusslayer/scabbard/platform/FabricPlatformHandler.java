77c58c0962463b9ff1a1063ed51402563e61a808
InvictusSlayer <ojsutters@gmail.com>
2025-08-24 18:41:37 +0100

    Small fabric fix

--- fabric/src/main/java/net/invictusslayer/scabbard/platform/FabricPlatformHandler.java
+++ fabric/src/main/java/net/invictusslayer/scabbard/platform/FabricPlatformHandler.java
@@ -33,25 +33,25 @@ public class FabricPlatformHandler implements IPlatformHandler {
 
 	@Override
 	public void addSpawnBiomeModifier(BiomeModifierHandler handler, String name, TagKey<Biome> biomes, List<MobSpawnSettings.SpawnerData> spawners) {
-		BiomeModification modification = BiomeModifications.create(ResourceLocation.fromNamespaceAndPath(Scabbard.MOD_ID, name));
+		BiomeModification modification = BiomeModifications.create(new ResourceLocation(Scabbard.MOD_ID, name));
 		spawners.forEach(spawner -> modification.add(ModificationPhase.ADDITIONS, context -> context.hasTag(biomes), context -> context.getSpawnSettings().addSpawn(spawner.type.getCategory(), spawner)));
 	}
 
 	@Override
 	public void addFeatureBiomeModifier(BiomeModifierHandler handler, String name, TagKey<Biome> biomes, GenerationStep.Decoration step, List<ResourceKey<PlacedFeature>> features) {
-		BiomeModification modification = BiomeModifications.create(ResourceLocation.fromNamespaceAndPath(Scabbard.MOD_ID, name));
+		BiomeModification modification = BiomeModifications.create(new ResourceLocation(Scabbard.MOD_ID, name));
 		features.forEach(feature -> modification.add(ModificationPhase.ADDITIONS, context -> context.hasTag(biomes), context -> context.getGenerationSettings().addFeature(step, feature)));
 	}
 
 	@Override
 	public <T> Supplier<T> register(Registry<? super T> registry, String modId, String name, Supplier<T> value) {
-		T registered = Registry.register(registry, ResourceLocation.fromNamespaceAndPath(modId, name), value.get());
+		T registered = Registry.register(registry, new ResourceLocation(modId, name), value.get());
 		return () -> registered;
 	}
 
 	@Override
 	public <T> Supplier<Holder.Reference<T>> registerHolder(Registry<T> registry, String modId, String name, Supplier<T> value) {
-		Holder.Reference<T> registered = Registry.registerForHolder(registry, ResourceLocation.fromNamespaceAndPath(modId, name), value.get());
+		Holder.Reference<T> registered = Registry.registerForHolder(registry, new ResourceLocation(modId, name), value.get());
 		return () -> registered;
 	}
 }
