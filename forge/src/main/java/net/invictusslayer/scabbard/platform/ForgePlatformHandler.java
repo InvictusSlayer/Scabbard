6480e1db7b6137b3ffd5f07c18c93fa8f923cddc
InvictusSlayer <ojsutters@gmail.com>
2025-08-25 12:51:09 +0100

    Added spawn egg registration to platform handler Minor WoodFamily lang fix

--- forge/src/main/java/net/invictusslayer/scabbard/platform/ForgePlatformHandler.java
+++ forge/src/main/java/net/invictusslayer/scabbard/platform/ForgePlatformHandler.java
@@ -4,12 +4,18 @@ import it.unimi.dsi.fastutil.objects.Reference2ObjectOpenHashMap;
 import net.invictusslayer.scabbard.world.biome.BiomeModifierHandler;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.SpawnEggItem;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.registries.DeferredRegister;
@@ -42,6 +48,11 @@ public class ForgePlatformHandler implements IPlatformHandler {
 	}
 
 	private static final Map<ResourceKey<?>, DeferredRegister> REGISTERS = new Reference2ObjectOpenHashMap<>();
+
+	@Override
+	public Supplier<SpawnEggItem> registerSpawnEgg(String modId, String name, Supplier<EntityType<? extends Mob>> entity, int bgColor, int fgColor, Item.Properties props) {
+		return register(BuiltInRegistries.ITEM, modId, name, () -> new ForgeSpawnEggItem(entity, bgColor, fgColor, props));
+	}
 
 	@Override
 	public <T> Supplier<T> register(Registry<? super T> registry, String modId, String name, Supplier<T> value) {
