e031757f8fdd77dfaea53f51ba9d9e86a91c2e09
InvictusSlayer <invictusslayer@gmail.com>
2026-07-19 23:00:43 +0100

    Removed model builder extension

--- neoforge/src/main/java/net/invictusslayer/scabbard/client/model/IExtendedModelBuilder.java
+++
@@ -1,7 +0,0 @@
-package net.invictusslayer.scabbard.client.model;
-
-import net.neoforged.neoforge.client.model.generators.ModelBuilder;
-
-public interface IExtendedModelBuilder<T extends ModelBuilder<T>> {
-	T textureSize(int width, int height);
-}
