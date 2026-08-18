d506cd46fb546fa8956f4b3f45808d6d35ecf616
InvictusSlayer <invictusslayer@gmail.com>
2026-08-18 15:57:23 +0100

    Fabric crash fix

--- fabric/src/main/java/net/invictusslayer/scabbard/mixin/client/FaceBakeryMixin.java
+++ fabric/src/main/java/net/invictusslayer/scabbard/mixin/client/FaceBakeryMixin.java
@@ -2,6 +2,7 @@ package net.invictusslayer.scabbard.mixin.client;
 
 import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.VertexFormatElement;
 import net.invictusslayer.scabbard.client.model.IExtendedBlockElementFace;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.block.model.BlockElementFace;
@@ -16,14 +17,20 @@ public class FaceBakeryMixin {
     @Unique
     private static final int STRIDE = DefaultVertexFormat.BLOCK.getIntegerSize();
     @Unique
-    private static final int UV2 = DefaultVertexFormat.BLOCK.offsets.getInt(DefaultVertexFormat.BLOCK.getElements().indexOf(DefaultVertexFormat.ELEMENT_UV2)) / 4;
+    private static final int OFFSET = getOffset(DefaultVertexFormat.ELEMENT_UV2);
 
     @ModifyArg(method = "bakeQuad", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/block/model/BakedQuad;<init>([IILnet/minecraft/core/Direction;Lnet/minecraft/client/renderer/texture/TextureAtlasSprite;Z)V"))
     private int[] setLightEmission(int[] vertices, @Local(argsOnly = true) BlockElementFace face) {
         int value = ((IExtendedBlockElementFace) face).scabbard$getLightEmission();
         for (int i = 0; i < 4; ++i) {
-            vertices[i * STRIDE + UV2] = LightTexture.pack(value, value);
+            vertices[i * STRIDE + OFFSET] = LightTexture.pack(value, value);
         }
         return vertices;
+    }
+
+    @Unique
+    private static int getOffset(VertexFormatElement element) {
+        int i = DefaultVertexFormat.BLOCK.getElements().indexOf(element);
+        return ((VertexFormatAccessor) DefaultVertexFormat.BLOCK).getOffsets().getInt(i) / 4;
     }
 }
